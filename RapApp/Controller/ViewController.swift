d9a8b45994006e5ab94e758d56c225b73547cb38
Michael Hayashi <mikephayashi@gmail.com>
2018-07-16 10:32:23 -0700

    Groups (Folders) Added
    Groups (Folders) Added

--- RapApp/Controller/ViewController.swift
+++ RapApp/Controller/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var randomWord: UILabel!
+    @IBOutlet weak var randomButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func randomButtonTapped(_ sender: Any) {
+        print("Generate Button Tapped")
+    }
+    
 }
 
