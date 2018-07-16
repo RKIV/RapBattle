d3330578be6515dc3acbecbf8cdd63464b8f8494
Michael Hayashi <mikephayashi@gmail.com>
2018-07-16 10:20:32 -0700

    Base
    Base

---
+++ RapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RapApp
+//
+//  Created by Michael Hayashi on 7/16/18.
+//  Copyright © 2018 Michael Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
