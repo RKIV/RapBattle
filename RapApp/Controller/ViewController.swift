7f22fdc7d6842fa2bae3048738d5072d2e5042ff
Michael Hayashi <mikephayashi@gmail.com>
2018-07-16 10:57:33 -0700

    Random Number
    Generate a Random Number when "Generate" is clicked

--- RapApp/Controller/ViewController.swift
+++ RapApp/Controller/ViewController.swift
@@ -11,21 +11,73 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    // MARK: Variables
+    
+    //Outlet Variables
     @IBOutlet weak var randomWord: UILabel!
     @IBOutlet weak var randomButton: UIButton!
+    
+    
+    //Order goes from left to right 1...4 then top to bottom 5...8
+    @IBOutlet weak var rhymeOne: UILabel!
+    @IBOutlet weak var rhymeTwo: UILabel!
+    @IBOutlet weak var rhymeThree: UILabel!
+    @IBOutlet weak var rhymeFour: UILabel!
+    @IBOutlet weak var rhymeFive: UILabel!
+    @IBOutlet weak var rhymeSix: UILabel!
+    @IBOutlet weak var rhymeSeven: UILabel!
+    @IBOutlet weak var rhymeEight: UILabel!
+    @IBOutlet weak var rhymeNine: UILabel!
+    @IBOutlet weak var rhymeTen: UILabel!
+    @IBOutlet weak var rhymeEleven: UILabel!
+    @IBOutlet weak var rhymeTwelve: UILabel!
+    @IBOutlet weak var rhymeThirteen: UILabel!
+    @IBOutlet weak var rhymeFourteen: UILabel!
+    @IBOutlet weak var rhymeFifteen: UILabel!
+    @IBOutlet weak var rhymeSixteen: UILabel!
+    
+    var rhymeWordsArray = [UILabel]()
+    
+    
+    // MARK: Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        rhymeWordsArray.append(rhymeOne)
+        rhymeWordsArray.append(rhymeTwo)
+        rhymeWordsArray.append(rhymeThree)
+        rhymeWordsArray.append(rhymeFour)
+        rhymeWordsArray.append(rhymeFive)
+        rhymeWordsArray.append(rhymeSix)
+        rhymeWordsArray.append(rhymeSeven)
+        rhymeWordsArray.append(rhymeEight)
+        rhymeWordsArray.append(rhymeNine)
+        rhymeWordsArray.append(rhymeTen)
+        rhymeWordsArray.append(rhymeEleven)
+        rhymeWordsArray.append(rhymeTwelve)
+        rhymeWordsArray.append(rhymeThirteen)
+        rhymeWordsArray.append(rhymeFourteen)
+        rhymeWordsArray.append(rhymeFifteen)
+        rhymeWordsArray.append(rhymeSixteen)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Generate Random Button Function
     @IBAction func randomButtonTapped(_ sender: Any) {
         print("Generate Button Tapped")
+        let randomWordNumber = arc4random_uniform(10)
+        randomWord.text = String(randomWordNumber)
+        for word in rhymeWordsArray {
+            let randomNumber = arc4random_uniform(10)
+            word.text = String(randomNumber)
+            print(randomNumber)
+        }
     }
     
 }
